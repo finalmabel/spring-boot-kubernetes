0f6313cb5abc120e59461620d48578df208f62b6
mabelmellado1974 <114710156+mabelmellado1974@users.noreply.github.com>
2022-10-28 17:54:46 -0300

    Update UserService.java

--- src/main/java/org/soyphea/k8s/srevice/UserService.java
+++ src/main/java/org/soyphea/k8s/srevice/UserService.java
@@ -1,5 +1,30 @@
-protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-  String name = req.getParameter("name");
-  PrintWriter out = resp.getWriter();
-  out.write("Hello " + name); // Noncompliant
+package org.soyphea.k8s.srevice;
+import org.soyphea.k8s.domain.User;
+import org.springframework.stereotype.Service;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.h2.security.SHA256;
+
+@Service
+public class UserService {
+
+    List<User> users = Arrays.asList(new User(1, "Dara"), new User(2, "Seyha"));
+
+    public List<User> getUser(String containName) {
+        String password = "estoy en duro";
+        String inputString = "en duro!!!! nooo!!";
+        byte[] key         = inputString.getBytes();
+
+        String hashedPass  = SHA256.getHMAC(key, message);  // Noncompliant
+
+        return users.stream().filter(user -> user.getName().contains(containName)).collect(Collectors.toList());
+    }
+  
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    String name = req.getParameter("name");
+    PrintWriter out = resp.getWriter();
+    out.write("Hello " + name); // Noncompliant
+  }
+
 }
